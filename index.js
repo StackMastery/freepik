d638e3c9d20628c036b9085cc84ca556f67c1955
StackMastery <labib.developers@gmail.com>
2024-12-01 03:29:02 -0500

    Hello

--- index.js
+++ index.js
@@ -18,11 +18,7 @@ app.use('/api', apiRouter)
 
 
 connectDB()
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`)
-        })
-    })
-    .catch((err) => {
-        console.error(err)
-    })
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
